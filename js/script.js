cbf7a0895dc92fc7670f04bc0ccd8beca8a36b27
radosimarmata27 <simarmatarado@gmail.com>
2023-10-02 13:57:59 +0700

    update

--- js/script.js
+++ js/script.js
@@ -10,3 +10,22 @@ $('.page-scroll').on('click', function (e) {
 
     e.preventDefault();
 });
+
+$(document).ready(function() {
+    $(window).on('scroll', function() {
+        var scrollPosition = $(this).scrollTop();
+        
+        $('a.page-scroll').each(function() {
+        var targetId = $(this).attr('href');
+        var targetPosition = $(targetId).offset().top - 70;
+        var targetHeight = $(targetId).height();
+        
+        if (scrollPosition >= targetPosition && scrollPosition < (targetPosition + targetHeight)) {
+            $('a.page-scroll').removeClass('active');
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+        });
+    });
+});
